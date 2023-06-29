ebb6a43c70cbad2e1cb8cb268d8ea3db733f988d
clezred <clezred.pro@gmail.com>
2023-06-29 21:59:02 +0200

    maj commande help

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 const { guildId } = require('../../config.json')
 
 module.exports = {
@@ -11,15 +11,18 @@ module.exports = {
             .addChoices(
                 {name: 'Pokédex', 	value: 'pokedex'},
                 {name: 'PokéLoto', 	value: 'pokeloto'},
+				{name: 'Team', 	value: 'team'},
                 //{name: 'PokéQuiz', 	value: 'pokequiz'},
                 //{name: 'PokéParty', value: 'pokeparty'},
+				{name: 'News Channel', value: 'news-channel'},
+				{name: 'Publish', value: 'publish'},
 				{name: 'Me', 		value: 'me'}
             )
         ),
 		
 	async execute(interaction) {
 
-		const choice = interaction.options.getString('commande') ?? 'basic'
+		const choice = interaction.options.getString('commande') ?? 'basic';
 
 		const pkbServField = {
 			name:"PokéBot Support Server",
@@ -31,7 +34,13 @@ module.exports = {
 
 			if (interaction.guild != null) {
 				if (interaction.guildId == guildId) {
-					guildCMDs = "\n- `/me`";
+					guildCMDs += "\n- `/me`";
+				}
+				if (interaction.member.permissions.has(PermissionFlagsBits.Administrator)) {
+					guildCMDs += "\n- `/news-channel`";
+				}
+				if (interaction.guildId == guildId && interaction.member.permissions.has(PermissionFlagsBits.Administrator)) {
+					guildCMDs += "\n - `/publish`";
 				}
 			}
 
@@ -144,7 +153,7 @@ module.exports = {
 				embeds: [{
 					title: "Aide commande Me",
 					color: 0xFFFF00,
-					description: "**Cette commande est exclusive au [Serveur Discord Officiel du PokéBot](https://discord.gg/FrMYzXn48V).",
+					description: "**Cette commande est exclusive au [Serveur Discord Officiel du PokéBot](https://discord.gg/FrMYzXn48V).**",
 					fields: [{
 							name: "Utilisation :",
 							value: "```/me```",
@@ -156,6 +165,78 @@ module.exports = {
 						},{
 							name: "Exemple :",
 							value: "Pour obtenir ton nouveau Pokémon tu peux utiliser :\n - `/me`"
+						}
+					],
+					footer: {text: "Aide demandée par " + interaction.user.username},
+					timestamp: new Date,
+				}],
+				ephemeral: true
+			})
+		} else if (choice == 'news-channel') {
+			interaction.reply({
+				embeds: [{
+					title: "Aide commande News Channel",
+					color: 0xFFFF00,
+					description: "**Cette commande est réservée aux administrateurs**",
+					fields: [{
+							name: "Utilisation :",
+							value: "```/news-channel ┬── add #channel\n              └── remove #channel```",
+							inline: false
+						},{
+							name: "Description :",
+							value: "Cette commande te permet d'ajouter ou de retirer un salon (de type textuel ou annonce) de la liste de diffusion des actualités du PokéBot.",
+							inline: false
+						},{
+							name: "Exemple :",
+							value: "Pour ajouter un salon à la liste :\n - `/news-channel add channel:<#id_du_salon>`\nPour retirer un salon de la liste :\n - `/news-channel remove channel:<#id_du_salon>`"
+						}
+					],
+					footer: {text: "Aide demandée par " + interaction.user.username},
+					timestamp: new Date,
+				}],
+				ephemeral: true
+			})
+		} else if (choice == 'publish') {
+			interaction.reply({
+				embeds: [{
+					title: "Aide commande Publish",
+					color: 0xFFFF00,
+					description: "**Cette commande est réservée aux administrateurs du [Serveur Discord Officiel du PokéBot](https://discord.gg/FrMYzXn48V).**",
+					fields: [{
+							name: "Utilisation :",
+							value: "```/publish {jsonFile}```",
+							inline: false
+						},{
+							name: "Description :",
+							value: "Cette commande permet de publier les actualités du PokéBot sur tous les salons ayant été ajouté à la liste de diffusion à l'aide d'un fichier JSON.\n*Voir la commande `/news-channel`*",
+							inline: false
+						},{
+							name: "Exemple :",
+							value: "Pour publier un nouveau message d'actualités :\n - `/publish {jsonFile}`\nMise en forme du fichier JSON :\n```JSON\n{\n\t\"content\": \"\",\n\t\"embeds\": [\n\t\t// structures d'embeds discord\n\t]\n}```"
+						}
+					],
+					footer: {text: "Aide demandée par " + interaction.user.username},
+					timestamp: new Date,
+				}],
+				ephemeral: true
+			})
+		} else if (choice == 'team') {
+			interaction.reply({
+				embeds: [{
+					title: "Aide commande Team",
+					color: 0xFFFF00,
+					description: "",
+					fields: [{
+							name: "Utilisation :",
+							value: "```/team```",
+							inline: false
+						},{
+							name: "Description :",
+							value: "Cette commande te permet d'obtenir une équipe de 6 Pokémons aléatoirement parmis les 1010 répertoriés.",
+							inline: false
+						},{
+							name: "Exemple :",
+							value: "Pour obtenir une équipe :\n - `/team`"
 						}
 					],
 					footer: {text: "Aide demandée par " + interaction.user.username},
